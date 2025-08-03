3bbee8e8aae0ea541ad2b3dc6ae2fe5f77d5374b
Camilo Estrada <estradasalinasjosecamilo@gmail.com>
2025-08-03 13:02:37 -0500

    Ajuste UI

---
+++ src/settings/site.js
@@ -0,0 +1,9 @@
+export const site = {
+  name: "Company Name",
+  description: "Lorem ipsum dolor sit amet consectetur",
+  social: {
+    facebook: "https://www.facebook.com/companyname",
+    instagram: "https://www.instagram.com/companyname",
+    linkedin: "https://www.linkedin.com/company/companyname",
+  },
+};
